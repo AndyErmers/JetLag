fd23ec56cae683030c1f0d1fecb28ac730f3c31a
AndyErmers <96414877+AndyErmers@users.noreply.github.com>
2024-08-01 11:07:27 +0200

    Voeg kaart toe

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    // Code om de kaart en leaderboard te initialiseren
+    var map = L.map('map').setView([50.85, 5.95], 10);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    // Voeg markers toe voor de bezienswaardigheden
+    fetch('data/challenges.json')
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(challenge => {
+                L.marker([challenge.lat, challenge.lon]).addTo(map)
+                    .bindPopup(`<b>${challenge.location}</b><br>${challenge.challenge}`);
+            });
+        });
 });
